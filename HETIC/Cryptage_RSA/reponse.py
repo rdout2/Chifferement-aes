ae86209f439ecc4132183446591c2e510ee80a7e
ahmat1 <rahmatmahamatdout@gmail.com>
2024-12-20 10:02:10 +0100

    correciton

--- HETIC/Cryptage_RSA/reponse.py
+++ HETIC/Cryptage_RSA/reponse.py
@@ -4,11 +4,8 @@ from Crypto.Random import get_random_bytes
 # Générer une clé AES de 256 bits
 key = get_random_bytes(32)
 key = "key.bin"
-# Chiffrement du message
-def encrypt_message(message, key):
-cipher = AES.new(key, AES.MODE_CBC)
-ciphertext = cipher.encrypt(pad(message.encode(), AES.block_size))
-return cipher.iv + ciphertext
+
+
 # Déchiffrement du message
 def decrypt_message(ciphertext, key):
     iv = ciphertext[:16]
@@ -16,10 +13,7 @@ def decrypt_message(ciphertext, key):
     plaintext = unpad(cipher.decrypt(ciphertext[16:]),
 AES.block_size).decode()
     return plaintext
-# Exemple d'utilisation
-message = "Voici un message crypté avec AES!"
-ciphertext = encrypt_message(message, key)
-print("Message chiffré:", ciphertext)
+
 # Lecture de la clé et du message chiffré à partir des fichiers
 with open("key.bin", "rb") as fichier:
     key_read = fichier.read()
